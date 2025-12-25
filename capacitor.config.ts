fd0bf4fca72dd94842202b57272926175936bd47
unknown <jiteshvarade1@gmail.com>
2025-12-25 18:58:01 +0530

    Fix: Update capacitor.config.ts with correct Bundle ID for CI/CD

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.nexthealth.app',
+  appId: 'com.cinfinite.nexthealth.app',
   appName: 'NextHealthApp',
   webDir: 'out'
 };
